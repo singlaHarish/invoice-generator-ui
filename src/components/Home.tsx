d740a4fe8880f847b01a3739cf017718b2fa6206
Harish Singla <harishsingla89@gmail.com>
2024-10-03 13:09:47 +0200

    some changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,8 +4,8 @@ import Layout from "./Layout";
 const Home = () => {
     return (
         <Layout>
-            <p>Welcome to our easy-to-use invoice generator, designed specifically for small shopkeepers.
-                With our tool, you can quickly create proessional invoices by simply inputting the items purchased by your customers. 
+            <p>Welcome to our easy-to-use invoice generator, designed specifically for small shopkeepers.<br/><br/>
+               With our tool, you can quickly create proessional invoices by simply inputting the items purchased by your customers. 
                 Once completed, you can generate a PDF version of the invoice, ready for printing or sharing digitally. 
                 Say goodbye to the hassle of manual paperwork and focus on what matters most running your business. 
                 Streamline your invoicing process with us today!</p>
