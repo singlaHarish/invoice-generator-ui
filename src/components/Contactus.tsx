4712e3de998302fe33133083a9eb8047931e986d
Harish Singla <harishsingla89@gmail.com>
2024-10-12 23:59:58 +0200

    contact us component

--- src/components/Contactus.tsx
+++ src/components/Contactus.tsx
@@ -1,13 +1,21 @@
 import React from "react"
+import { Col, Container, Row } from "react-bootstrap"
 
 const Contactus = () => {
     return (
-        <div>
-            <h2>Contact Us</h2>
-            <p>If you have any questions or need support, feel free to reach us at:</p>
-            <p>
-                <strong>Email:</strong> <a href="mailto:harishsingla89@gmail.com">harishsingla89@gmail.com</a>
-            </p>
+        <div className="contactus">
+            <Container className="mt-5 content">
+                <Row>
+                    <Col>
+                        <h2>Contact Us</h2>
+                        <p>If you have any questions or need support, feel free to reach us at:</p>
+                        <p>
+                            <strong>Email:</strong> <a href="mailto:harishsingla89@gmail.com">harishsingla89@gmail.com</a>
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,13 +8,13 @@ const Home = () => {
                 <Row>
                     <Col>
                         <h2>Welcome to our easy-to-use invoice generator, designed specifically for small shopkeepers!</h2>
+                        <p className="welcomepage">
+                            With our tool, you can quickly create proessional invoices by simply inputting the items purchased by your customers.
+                            Once completed, you can generate a PDF version of the invoice, ready for printing or sharing digitally.
+                            Say goodbye to the hassle of manual paperwork and focus on what matters most running your business.
+                            Streamline your invoicing process with us today!
+                        </p>
                     </Col>
-                    <p className="welcomepage">
-                        With our tool, you can quickly create proessional invoices by simply inputting the items purchased by your customers.
-                        Once completed, you can generate a PDF version of the invoice, ready for printing or sharing digitally.
-                        Say goodbye to the hassle of manual paperwork and focus on what matters most running your business.
-                        Streamline your invoicing process with us today!
-                    </p>
                 </Row>
             </Container>
         </div>
